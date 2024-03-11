40b4cdc5656da65201c29bc6d87af943d4f828c8
hungify <nmhungify@gmail.com>
2024-03-11 23:48:19 +0700

    refactor: add openapi-types package

---
+++ packages/openapi-types/scripts/index.ts
@@ -0,0 +1,85 @@
+import { readdir, mkdir, writeFile } from "node:fs/promises";
+import { basename } from "node:path";
+
+import * as prettier from "prettier";
+import openapiTS from "openapi-typescript";
+
+run();
+
+const packageDefaults = {
+  publishConfig: {
+    access: "public",
+  },
+  version: "14.0.0",
+  main: "",
+  types: "types.d.ts",
+  author: "hungify",
+  license: "MIT",
+  octokit: {
+    "openapi-version": "3.0.3",
+  },
+};
+
+async function run() {
+  const files = await readdir("cache");
+  for (const fileName of files) {
+    if (!/\.json$/.test(fileName)) continue;
+
+    const name = basename(fileName, ".json");
+
+    const packageName =
+      name === "api.github.com" ? "openapi-types" : `openapi-types-${name}`;
+    await mkdir(`packages/${packageName}`);
+    await writeFile(
+      `packages/${packageName}/package.json`,
+      await prettier.format(
+        JSON.stringify({
+          name: `@octokit/${packageName}`,
+          description: `Generated TypeScript definitions based on GitHub's OpenAPI spec for ${name}`,
+          repository: {
+            type: "git",
+            url: "https://github.com/octokit/openapi-types.ts.git",
+            directory: `packages/${packageName}`,
+          },
+          ...packageDefaults,
+        }),
+        { parser: "json-stringify" }
+      )
+    );
+    await writeFile(
+      `packages/${packageName}/README.md`,
+      await prettier.format(
+        `
+# @octokit/${packageName}
+
+> Generated TypeScript definitions based on GitHub's OpenAPI spec ${
+          packageName === "openapi-types" ? "" : `for ${name}`
+        }
+
+This package is continuously updated based on [GitHub's OpenAPI specification](https://github.com/github/rest-api-description/)
+
+## Usage
+
+\`\`\`ts
+import { components } from "@octokit/${packageName}";
+
+type Repository = components["schemas"]["full-repository"]
+\`\`\`
+
+## License
+
+[MIT](LICENSE)
+`,
+        { parser: "markdown" }
+      )
+    );
+
+    await writeFile(
+      `packages/${packageName}/types.d.ts`,
+      await prettier.format(await openapiTS(`cache/${name}.json`), {
+        parser: "typescript",
+      })
+    );
+    console.log(`packages/${packageName}/types.d.ts written`);
+  }
+}
